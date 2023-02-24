57722abcd357460c887a1285e70be5ea8480e7ce
Terracotta-Lite <terracottalite@gmail.com>
2023-02-24 22:54:00 +0300

    add arguments to lcm-pairs.c

--- lcm-pairs.c
+++ lcm-pairs.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 /*
 Gets the gcd of a and b
@@ -27,31 +28,36 @@ int lcm(int a, int b) {
 int pairsSmallerThanM(int m) {
 	int abg,k;
 	int n = 2;
-	int count = 0;
+	int count;
+	int result = 0;
+
 	while ( n < m + 1 ) {
-		k = 2;
+		k = n+1;
+		count = 0;
 		while ( k < m + 1 ) {
-			if (k != n) {
-				abg = lcm(n, k);
-				if (abg < m + 1 & abg > 1) count++;
-			}
+			abg = lcm(n, k);
+			if (abg < m + 1) count++;
 			k++;
 		}
+		result += count;  
 		n++;
 	}
-	return count / 2;
+	return result;
 }
 
-int main() {
-	int m = 210;
-	int l = 1;
+int main(int argc, char *argv[]) {
+	if (argc < 3) { printf("ERROR: Not enough arguments, expected at least 2\n"); return 1; }
+	int m = atoi(argv[2]);
+	int l = atoi(argv[1]);
 	int c1;
 	int c2 = 0;
+
 	while (l < m + 1) {
 		c1 = pairsSmallerThanM(l);
 		printf("%d: %d, increase: %d\n", l, c1, c1 - c2);
 		c2 = c1;
 		l++;
 	}
+
 	return 0;
 }
